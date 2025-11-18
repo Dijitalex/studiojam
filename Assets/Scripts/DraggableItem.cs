65d0cf190fda9f2546c3be83afd956e87a4bfdd3
MaggieQiang <maggieqiang717@gmail.com>
2025-11-17 16:46:55 -0800

    3 trays + 3 food prefab images added

--- Assets/Scripts/DraggableItem.cs
+++ Assets/Scripts/DraggableItem.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
-
-public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+using UnityEngine.UI;
+public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerDownHandler
 {
     public RectTransform trayRect; 
     public RectTransform dropAreaRect; 
@@ -11,11 +11,20 @@ public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     private Canvas parentCanvas;
 
     private bool dragging = false;
+    private Image foodImage;
 
     void Start()
     {
         parentCanvas = GetComponentInParent<Canvas>();
         trayHomePos = transform.position;
+
+        foodImage = GetComponent<Image>();
+        if (foodImage != null)
+        {
+            Color color = foodImage.color;
+            color.a = 0f;
+            foodImage.color = color;
+        }
     }
 
     void Update()
@@ -36,9 +45,26 @@ public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
         }
     }
 
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown called!");
+        if (foodImage != null)
+        {
+            Color color = foodImage.color;
+            color.a = 1f;
+            foodImage.color = color;
+        }
+    }
+    
     public void OnBeginDrag(PointerEventData eventData)
     {
         dragging = true;
+        if (foodImage != null)
+        {
+            Color color = foodImage.color;
+            color.a = 1f;
+            foodImage.color = color;
+        }
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -46,19 +72,39 @@ public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
         transform.position = Input.mousePosition;
     }
 
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        dragging = false; 
+public void OnEndDrag(PointerEventData eventData)
+{
+    dragging = false; 
 
-        Camera cam = parentCanvas.renderMode == RenderMode.ScreenSpaceOverlay
-            ? null : parentCanvas.worldCamera;
+    Camera cam = parentCanvas.renderMode == RenderMode.ScreenSpaceOverlay
+        ? null : parentCanvas.worldCamera;
 
-        if (RectTransformUtility.RectangleContainsScreenPoint(dropAreaRect, eventData.position, cam))
+    if (RectTransformUtility.RectangleContainsScreenPoint(dropAreaRect, eventData.position, cam))
+    {    
+        if (foodPrefab == null)
         {
-            Vector3 worldPos = Camera.main.ScreenToWorldPoint(eventData.position);
-            worldPos.z = 0;
-            Instantiate(foodPrefab, worldPos, Quaternion.identity);
+            return;
         }
-        transform.position = trayHomePos;
+        
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(eventData.position);
+        worldPos.z = 0;
+        
+        GameObject newFood = Instantiate(foodPrefab, worldPos, Quaternion.identity);
+        
+        SpriteRenderer sr = newFood.GetComponent<SpriteRenderer>();
+        if (sr != null)
+        {
+            sr.sortingOrder = 100;
+        }
     }
+    
+    transform.position = trayHomePos;
+    
+    if (foodImage != null)
+    {
+        Color color = foodImage.color;
+        color.a = 0f;
+        foodImage.color = color;
+    }
+}
 }
